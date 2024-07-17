5488a0a4da3b658d18b646675cedbe45ef68a5b9
이탁균 <vbmrkk@naver.com>
2024-07-17 14:51:52 +0900

    Update CreateJunction.ps1

--- CreateJunction.ps1
+++ CreateJunction.ps1
@@ -1,10 +1,14 @@
 param (
-    [string]$JunctionPath,
+    [string]$JunctionBasePath,
     [string]$TargetPath
 )
 
-if (Test-Path $JunctionPath) {
-    Remove-Item $JunctionPath -Recurse -Force
+# Get the name of the current directory
+$currentDirName = Split-Path -Leaf $TargetPath
+$junctionPath = Join-Path -Path $JunctionBasePath -ChildPath $currentDirName
+
+if (Test-Path $junctionPath) {
+    Remove-Item $junctionPath -Recurse -Force
 }
 
-New-Item -ItemType Junction -Path $JunctionPath -Target $TargetPath
+New-Item -ItemType Junction -Path $junctionPath -Target $TargetPath
