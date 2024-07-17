40d43b8fc5e655ceea4e1848a79dea0433fca676
이탁균 <vbmrkk@naver.com>
2024-07-17 14:47:13 +0900

    Update CreateJunction.bat

--- CreateJunction.bat
+++ CreateJunction.bat
@@ -2,11 +2,11 @@
 :: Detect if the script is running in PowerShell
 setlocal EnableDelayedExpansion
 set psDetect=%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe
-for /f "tokens=*" %%i in ('%psDetect% -Command "echo 1"') do set psCheck=%%i
+for /f "tokens=*" %%i in ('%psDetect% -Command "Write-Output 1') do set psCheck=%%i
 
 if "%psCheck%"=="1" (
     :: Running in PowerShell
-    %SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -File CreateJunction.ps1 -JunctionPath C:\src -TargetPath "%cd%"
+    %SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -File "CreateJunction.ps1" -JunctionPath "C:\src" -TargetPath "%cd%"
 ) else (
     :: Running in CMD
     set target=%cd%
