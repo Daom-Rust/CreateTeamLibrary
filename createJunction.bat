ad1bdc973d79d50db3b9391055931a42e3bf0597
이탁균 <vbmrkk@naver.com>
2024-07-17 14:45:17 +0900

    Update createJunction.bat

--- createJunction.bat
+++ createJunction.bat
@@ -6,14 +6,7 @@ for /f "tokens=*" %%i in ('%psDetect% -Command "echo 1"') do set psCheck=%%i
 
 if "%psCheck%"=="1" (
     :: Running in PowerShell
-    %SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& {
-        $target = Get-Location;
-        $junctionPath = 'C:\src';
-        if (Test-Path $junctionPath) {
-            Remove-Item $junctionPath -Recurse -Force;
-        }
-        New-Item -ItemType Junction -Path $junctionPath -Target $target;
-    }"
+    %SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -File CreateJunction.ps1 -JunctionPath C:\src -TargetPath "%cd%"
 ) else (
     :: Running in CMD
     set target=%cd%
